a70491aa58584524bf0bc7b8f923d92ab1abfb66
Monika Mona <mona.mt.89@gmail.com>
2022-05-12 11:22:07 +0200

    material-ui&react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import * as React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +21,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Dashboard></Dashboard>
     </div>
+    </>
   );
 }
 
